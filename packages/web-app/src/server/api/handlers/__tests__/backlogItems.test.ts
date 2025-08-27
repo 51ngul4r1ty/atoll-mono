83d40fba25c61a40641bc4d94d329a8c35817a0c
kevin <kevinbe71@gmail.com>
2025-08-26 23:16:41 -0400

    test scenario related to bug fix

--- packages/web-app/src/server/api/handlers/__tests__/backlogItems.test.ts
+++ packages/web-app/src/server/api/handlers/__tests__/backlogItems.test.ts
@@ -204,5 +204,176 @@ describe("Backlog Items", () => {
                 }
             ]);
         });
+        /**
+         * This test is a special case because the end of the list is designated
+         * by a null link.  The reorder operation inserts the source item BEFORE
+         * the target provided.  So, in order to add to the end of the list the
+         * target provided will be null, but that can match any of the
+         * "end of list" items because there could be more than one project in
+         * the list.  To simulate this properly 3 projects are included.
+         */
+        it("should handle reorder scenario to end of list", async () => {
+            const project0Items = [
+                {
+                    backlogitemId: null,
+                    nextbacklogitemId: "X",
+                    projectId: "PROJECT_0"
+                },
+                {
+                    backlogitemId: "X",
+                    nextbacklogitemId: "Y",
+                    projectId: "PROJECT_0"
+                },
+                {
+                    backlogitemId: "Y",
+                    nextbacklogitemId: "Z",
+                    projectId: "PROJECT_0"
+                },
+                {
+                    backlogitemId: "Z",
+                    nextbacklogitemId: null,
+                    projectId: "PROJECT_0"
+                }
+            ];
+            const project2Items = [
+                {
+                    backlogitemId: null,
+                    nextbacklogitemId: "P",
+                    projectId: "PROJECT_2"
+                },
+                {
+                    backlogitemId: "P",
+                    nextbacklogitemId: "Q",
+                    projectId: "PROJECT_2"
+                },
+                {
+                    backlogitemId: "Q",
+                    nextbacklogitemId: "R",
+                    projectId: "PROJECT_2"
+                },
+                {
+                    backlogitemId: "R",
+                    nextbacklogitemId: null,
+                    projectId: "PROJECT_2"
+                }
+            ];
+            mockDatabase.addCollection("productbacklogitems", [
+                ...project0Items,
+                {
+                    backlogitemId: null,
+                    nextbacklogitemId: "A",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "A",
+                    nextbacklogitemId: "B",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "B",
+                    nextbacklogitemId: "C",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "C",
+                    nextbacklogitemId: "D",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "D",
+                    nextbacklogitemId: "E",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "E",
+                    nextbacklogitemId: "F",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "F",
+                    nextbacklogitemId: null,
+                    projectId: "PROJECT_1"
+                },
+                ...project2Items
+            ]);
+            // arrange
+            const req: Request = {
+                body: {
+                    sourceItemId: "B", // source is used to determine project ID
+                    targetItemId: null // move to end of list
+                }
+            } as any;
+            const res: Response = {
+                status: statusFn
+            } as any;
+            const findOneSpy = jest.spyOn(ProductBacklogItemDataModel, "findOne");
+            const updateSpy = jest.spyOn(ProductBacklogItemDataModel, "update");
+            updateSpy.mockImplementation(() => null);
+            findOneSpy.mockImplementation((options) => {
+                const item = mockDatabase.getCollectionItemValueByValues("productbacklogitems", options.where);
+                return Promise.resolve({
+                    dataValues: {
+                        ...item
+                    },
+                    update: (updateValues, options) => {
+                        mockDatabase.updateItem("productbacklogitems", item, updateValues);
+                    }
+                } as any);
+            });
+
+            // act
+            await backlogItemsReorderPostHandler(req, res);
+
+            // assert
+            expect(findOneSpy).toBeCalledTimes(3);
+            expect(sequelizeTransactionCommmitMock).toBeCalledTimes(1);
+            expect(sequelizeTransactionRollbackMock).toBeCalledTimes(0);
+            const allCollectionValuesRaw = mockDatabase.getAllCollectionValues("productbacklogitems");
+            const allCollectionValues = sortLinkedListArray(
+                allCollectionValuesRaw,
+                "backlogitemId",
+                "nextbacklogitemId",
+                "projectId"
+            );
+            expect(allCollectionValues).toStrictEqual([
+                ...project0Items,
+                {
+                    backlogitemId: null,
+                    nextbacklogitemId: "A",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "A",
+                    nextbacklogitemId: "C",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "C",
+                    nextbacklogitemId: "D",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "D",
+                    nextbacklogitemId: "E",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "E",
+                    nextbacklogitemId: "F",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "F",
+                    nextbacklogitemId: "B",
+                    projectId: "PROJECT_1"
+                },
+                {
+                    backlogitemId: "B",
+                    nextbacklogitemId: null,
+                    projectId: "PROJECT_1"
+                },
+                ...project2Items
+            ]);
+        });
     });
 });
