43633242b5a1b914c16413c7243c8182b4f3efdf
kevin <kevinbe71@gmail.com>
2026-04-29 23:18:33 -0400

    Uncomment code

--- packages/shared/src/components/atoms/info/Pill.tsx
+++ packages/shared/src/components/atoms/info/Pill.tsx
@@ -16,9 +16,9 @@ export type PillProps = PillStateProps & PillDispatchProps;
 /* exported components */
 
 export const Pill: React.FC<PillProps> = (props) => {
-    // if (typeof props.children !== "string" && typeof props.children !== "number") {
-    //     return <div className={css.invalidPillContent}>(invalid)</div>;
-    // }
+    if (typeof props.children !== "string" && typeof props.children !== "number") {
+        return <div className={css.invalidPillContent}>(invalid)</div>;
+    }
 
     return <div className={buildClassName(css.pill, props.className)}>{props.children}</div>;
 };
